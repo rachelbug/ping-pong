fa19c88016effa97261bf03fa8b787cee20c5fa2
Rachel Etnire <rachel.etnire@gmail.com>
2016-10-06 10:33:31 -0700

    taking a break, code is still WIP

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,23 @@ $(document).ready(function() {
 // Ping: (userInput % 3 === 0)
 // Pong: (userInput % 5 === 0)
 // PingPong: (userInput % 15 === 0)
+
+// for (i = 0; i <= userValue; i++) {
+//   if ((i % 3) === 0) {
+//     push("ping");
+//   } else if ((i % 5) ===0) {
+//     push("pong");
+//   } else if ((i % 15) === 0) {
+//     push("pingpong");
+//   } else {
+//     alert("Please enter a number!");
+//   }
+// }
+
+// Make the output a string
+
+// var output = "";
+// for (i = 1; i <= 5; i++) {
+//   output += i.toString() + " ";
+// }
+// alert(output);
