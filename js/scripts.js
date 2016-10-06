0f767910a4d049b1330f40aa10d2a19c50eb0494
Rachel Etnire <rachel.etnire@gmail.com>
2016-10-06 10:15:01 -0700

    add initial js to ping pong application

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var numbers =[];
+
+
+$(document).ready(function() {
+  $("form#pingpong").submit(function(event) {
+    event.preventDefault();
+
+    var userInput = parseInt($("input#numberInput").val());
+
+
+    $("#result").text(userInput);
+    $("#result").show();
+
+  });
+});
+
+// 
+// Ping: (userInput % 3 === 0)
+// Pong: (userInput % 5 === 0)
+// PingPong: (userInput % 15 === 0)
