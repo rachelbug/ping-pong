0d0c3ece1a493e9c766842d43ed908790a8f34fb
Rachel Etnire <rachel.etnire@gmail.com>
2016-10-07 16:20:07 -0700

    additional work on JS to try and satisfy second spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // var numbers = [];
 //
 // // pingPongFunction = function(input) {
-// //   for (i = 0; i <= userValue; i++) {
+// //   for (var i = 1;i <= userValue; i++) {
 // //     if ((i % 3) === 0) {
 // //       numbers.push("ping");
 // //     } else if ((i % 5) ===0) {
@@ -9,32 +9,30 @@
 // //     } else if ((i % 15) === 0) {
 // //       numbers.push("pingpong");
 // //     } else {
-// //       alert("Please enter a number!");
+//       numbers.push(i);
 // //     }
 // //   }
 // // }
 
+// var numbers = [];
+//
+// pingPongFunction = function(input) {
+//   for (var i = 1;i <= input; i++) {
+//     numbers.push(i);
+//     }
+//  numbers += i.toSlice();
+    alert(numbers);
+//   }
 
 $(document).ready(function() {
   $("form#pingpong").submit(function(event) {
     event.preventDefault();
 
     var userInput = $("input#userValue").val();
+    // var result = pingPongFunction(userValue);
 
-// I have the user input value, I need to make this into an array where all of the array is returned.
-
-    var newUserInput = userInput.toString() + " ";
-
-    $("#result").text(newUserInput);
+    $("#result").text(userInput);
     $("#result").show();
 
   });
 });
-
-// Make the output a string
-
-// var output = "";
-// for (i = 1; i <= 5; i++) {
-//   output += i.toString() + " ";
-// }
-// alert(output);
