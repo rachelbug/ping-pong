4bdcddd66282897ffd7bbfc5701c86b998f246f5
Rachel Etnire <rachel.etnire@gmail.com>
2016-10-08 11:42:31 -0700

    add logic to satisfy first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,21 @@
-// var numbers = [];
-//
-// // pingPongFunction = function(input) {
-// //   for (var i = 1;i <= userValue; i++) {
-// //     if ((i % 3) === 0) {
-// //       numbers.push("ping");
-// //     } else if ((i % 5) ===0) {
-// //       numbers.push("pong");
-// //     } else if ((i % 15) === 0) {
-// //       numbers.push("pingpong");
-// //     } else {
-//       numbers.push(i);
-// //     }
-// //   }
-// // }
+pingPong = function(num) {
 
-// var numbers = [];
-//
-// pingPongFunction = function(input) {
-//   for (var i = 1;i <= input; i++) {
-//     numbers.push(i);
-//     }
-//  numbers += i.toSlice();
-    alert(numbers);
-//   }
+  var numbers = [];
+
+  for (var i = 1;i <= num; i++) {
+      numbers.push(i);
+    }
+  return numbers.join(", ");
+}
 
 $(document).ready(function() {
   $("form#pingpong").submit(function(event) {
     event.preventDefault();
 
     var userInput = $("input#userValue").val();
-    // var result = pingPongFunction(userValue);
+    var result = pingPong(userInput);
 
-    $("#result").text(userInput);
+    $("#result").text(result);
     $("#result").show();
 
   });
